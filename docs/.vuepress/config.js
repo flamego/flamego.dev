a259329f081fd57df67b63d97ef671dd5db576e4
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2021-05-22 17:33:05 +0800

    Add vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /**
+     * Ref：https://v1.vuepress.vuejs.org/config/#title
+     */
+    title: 'Flamego',
+    /**
+     * Ref：https://v1.vuepress.vuejs.org/config/#description
+     */
+    description: 'A fantastic modular Go web framework boiled with black magic',
+
+    /**
+    * Extra tags to be injected to the page HTML `<head>`
+    *
+    * ref：https://v1.vuepress.vuejs.org/config/#head
+    */
+    head: [
+        ['link', { rel: "icon", href: "/favicon.png"}]
+    ],
+}
