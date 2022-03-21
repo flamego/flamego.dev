91ab5b1b174e6ccdc84e9e13f0db4ae7136be13a
Joe Chen <jc@unknwon.io>
2022-03-21 19:44:23 +0800

    Use DocSearch index

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -137,10 +137,9 @@ module.exports = {
         [
             "@vuepress/plugin-docsearch",
             {
-                apiKey: "3d0b6f791fd53ca9e72b76121882a3c5",
-                appId: "H5Z5B7U47X",
-                indexName:
-                    "netlify_8c6545f8-a001-4515-b052-63847be97752_main_all",
+                apiKey: "a255dc3bbb7f4982286e930e43161e08",
+                appId: "A5MMRNIDGB",
+                indexName: "flamego",
                 locales: {
                     "/": {
                         placeholder: "Search",
@@ -155,6 +154,41 @@ module.exports = {
                         translations: {
                             button: {
                                 buttonText: "搜索文档",
+                            },
+                            modal: {
+                                searchBox: {
+                                    resetButtonTitle: "清除查询条件",
+                                    resetButtonAriaLabel: "清除查询条件",
+                                    cancelButtonText: "取消",
+                                    cancelButtonAriaLabel: "取消",
+                                },
+                                startScreen: {
+                                    recentSearchesTitle: "搜索历史",
+                                    noRecentSearchesText: "没有搜索历史",
+                                    saveRecentSearchButtonTitle:
+                                        "保存至搜索历史",
+                                    removeRecentSearchButtonTitle:
+                                        "从搜索历史中移除",
+                                    favoriteSearchesTitle: "收藏",
+                                    removeFavoriteSearchButtonTitle:
+                                        "从收藏中移除",
+                                },
+                                errorScreen: {
+                                    titleText: "无法获取结果",
+                                    helpText: "你可能需要检查你的网络连接",
+                                },
+                                footer: {
+                                    selectText: "选择",
+                                    navigateText: "切换",
+                                    closeText: "关闭",
+                                    searchByText: "搜索提供者",
+                                },
+                                noResultsScreen: {
+                                    noResultsText: "无法找到相关结果",
+                                    suggestedQueryText: "你可以尝试查询",
+                                    openIssueText: "你认为该查询应该有结果？",
+                                    openIssueLinkText: "点击反馈",
+                                },
                             },
                         },
                     },
